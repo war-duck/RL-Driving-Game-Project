0df29f719b3d3385d01316ade90f4f8531341074
war-duck <01178893@pw.edu.pl>
2024-08-30 19:26:19 +0200

    Fixed error: "Duplicate assembly info"

--- Driving-Game/Code.Tests/obj/Debug/net8.0/Code.Tests.AssemblyInfo.cs
+++ Driving-Game/Code.Tests/obj/Debug/net8.0/Code.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a0783dc19353b52a1bcf088e953449a3bd74e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59cc15ecc190688c9883965bf956413f64b5cc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
